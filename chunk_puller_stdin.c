0ef8582903142a3f01df59d1e9856dc171f36f39
CsabaKiraly <CsabaKiraly@c54fac2e-1815-407e-9b96-088d06743913>
2010-03-30 06:35:15 +0000

    adding stdin chunkstream input
    git-svn-id: https://repository.napa-wine.eu/svn/napawine-software/trunk/Applications/StreamerPlayerChunker@812 c54fac2e-1815-407e-9b96-088d06743913

---
+++ chunk_puller_stdin.c
@@ -0,0 +1,41 @@
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 512000
+
+static void *receive(void *dummy)
+{
+  char msg[MAX_SIZE];
+  while (1) {
+    int32_t msg_size;
+    int32_t p = 0;
+    read(0, &msg, 5);
+    read(0, &msg_size, sizeof(msg_size));
+fprintf(stderr, "Reading chunk of size %d\n", msg_size);
+    if (msg_size > MAX_SIZE) {
+      fprintf(stderr, "chunk too big (%d bytes)\n", msg_size);
+      exit(1);
+    } else {
+      while (p <  msg_size) {
+        int r = read(0, &msg + p, msg_size - p);
+        if (r >= 0) {
+          p += r;
+        }
+      }
+      enqueueBlock(msg, msg_size); //this might take some time
+    }
+  }
+}
+
+void *initChunkPuller() {
+  pthread_t stdin_thread;
+  pthread_create(&stdin_thread, NULL, receive, NULL); 
+  pthread_join(stdin_thread, NULL);
+
+}
+
+void finalizeChunkPuller(void *d) {
+}
+
