5211f6590cb7c661aa5caccb7e14e97f419fb6d5
GiuseppeTropea <GiuseppeTropea@c54fac2e-1815-407e-9b96-088d06743913>
2010-04-18 00:42:28 +0000

    chunk IDs based also on start timestamp in milliseconds of chunk (configurabile via chunker.config)
    git-svn-id: https://repository.napa-wine.eu/svn/napawine-software/trunk/Applications/StreamerPlayerChunker@975 c54fac2e-1815-407e-9b96-088d06743913

--- chunk_pusher_nh.c
+++ chunk_pusher_nh.c
@@ -11,7 +11,11 @@
 #define MY_IP "127.0.0.1"
 #define MY_PORT 4444
 #define STREAMER_IP "127.0.0.1"
-#define STREAMER_PORT 8888
+#define STREAMER_PORT 6666
+
+#define DEBUG_PUSHER
+
+extern ChunkerMetadata *cmeta;
 
 struct nodeID *streamer;
 
@@ -41,8 +45,8 @@ int pushChunkHttp(ExternalChunk *echunk, char *url) {
 	uint8_t *buffer=NULL;
 	static size_t attr_size = 0;
 	size_t buffer_size = 0;
-	double ts1e3 = 0.0;
-	double tus3e1 = 0.0;
+	int ts1e3 = 0.0;
+	int tus3e1 = 0.0;
 	int ret = STREAMER_FAIL_RETURN;
 	
 	attr_size = 5*sizeof(int) + 2*sizeof(time_t) + 2*sizeof(suseconds_t) + 1*sizeof(double);
@@ -51,12 +55,28 @@ int pushChunkHttp(ExternalChunk *echunk, char *url) {
 	if(	(grapes_chunk_attributes_block = packExternalChunkToAttributes(echunk, attr_size)) != NULL ) {
 		/* then fill-up a proper GRAPES chunk */
 		Chunk gchunk;
-		gchunk.id = echunk->seq;
 		gchunk.size = echunk->payload_len;
 		/* convert external_chunk start_time in milliseconds and put it in grapes chunk */
-		ts1e3 = (double)(echunk->start_time.tv_sec*1e3);
-		tus3e1 = (double)(echunk->start_time.tv_usec/1e3);
-		gchunk.timestamp = ts1e3+tus3e1;
+		ts1e3 = (int)(echunk->start_time.tv_sec*1e3);
+		//tus3e1 = (int)(echunk->start_time.tv_usec/1e3); ALREADY IN MILLI SECONDS
+		gchunk.timestamp = ts1e3+(int)(echunk->start_time.tv_usec);
+#ifdef DEBUG_PUSHER
+			fprintf(stderr, "CHUNKER: start sec %d usec %d timestamp %ld\n", ts1e3, (int)(echunk->start_time.tv_usec), gchunk.timestamp);
+#endif
+
+		//decide how to create the chunk ID
+		if(cmeta->cid == 0) {
+			gchunk.id = echunk->seq;
+#ifdef DEBUG_PUSHER
+			fprintf(stderr, "CHUNKER: packaged SEQ chunkID %d\n", gchunk.id);
+#endif
+		}
+		else if(cmeta->cid == 1) {
+			gchunk.id = gchunk.timestamp; //its ID is its start time
+#ifdef DEBUG_PUSHER
+			fprintf(stderr, "CHUNKER: packaged TS chunkID %d\n", gchunk.id);
+#endif
+		}
 		gchunk.attributes = grapes_chunk_attributes_block;
 		gchunk.attributes_size = attr_size;
 		gchunk.data = echunk->data;
