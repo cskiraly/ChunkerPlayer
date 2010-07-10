88aed679a2ea62cee68e734001d0406c1f3e2956
GiuseppeTropea <GiuseppeTropea@c54fac2e-1815-407e-9b96-088d06743913>
2010-07-10 18:11:10 +0000

    switched UL to mpeg4 to do baseline tests
    git-svn-id: https://repository.napa-wine.eu/svn/napawine-software/trunk/Applications/StreamerPlayerChunker@1523 c54fac2e-1815-407e-9b96-088d06743913

--- codec_definitions.h
+++ codec_definitions.h
@@ -1,7 +1,7 @@
 #ifndef _CODEC_DEFINITIONS_H
 #define _CODEC_DEFINITIONS_H
 
-#define H264_VIDEO_ENCODER
-#define MP3_AUDIO_ENCODER
+//#define H264_VIDEO_ENCODER
+//#define MP3_AUDIO_ENCODER
 
 #endif
