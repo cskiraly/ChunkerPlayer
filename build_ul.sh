9f668f3273522faf6d2230e855cd0e7b66f566a9
GiuseppeTropea <GiuseppeTropea@c54fac2e-1815-407e-9b96-088d06743913>
2010-07-16 16:54:59 +0000

    UL build script: added download and compile of mp3lame. Stronger checks. Default names for dirs of extern libs.
    git-svn-id: https://repository.napa-wine.eu/svn/napawine-software/trunk/Applications/StreamerPlayerChunker@1580 c54fac2e-1815-407e-9b96-088d06743913

--- build_ul.sh
+++ build_ul.sh
@@ -4,18 +4,19 @@ BASE_UL_DIR=`dirname $SCRIPT`
 EXTERN_DIR="external_libs"
 cd "$BASE_UL_DIR"
 
-#try to find libbz2 and mp3lame in your system
-LOCAL_BZ2=`locate libbz2.a`
-if [ "$LOCAL_BZ2" = "" ]; then
-	echo "locate command could not find file libbz2.a"
+#try to find libbz2 in your system
+LOCAL_BZ2_A=`locate libbz2.a`
+if [ $LOCAL_BZ2_A = "" ]; then
+    if [ -f "/usr/lib/libbz2.a" ]; then
+	echo "You have file libbz2.a in default system"
 	echo "setting path for it to default /usr/lib/libbz2.a"
-	LOCAL_BZ2="/usr/liblib/bz2.a"
-fi
-LOCAL_MP3LAME=`locate libmp3lame.a`
-if [ "$LOCAL_MP3LAME" = "" ]; then
-	echo "locate command could not find file libmp3lame.a"
-	echo "setting path for it to default /usr/lib/libmp3lame.a"
-	LOCAL_MP3LAME="/usr/liblib/libmp3lame.a"
+	LOCAL_BZ2="/usr"
+    else
+	echo "you seem not to have file libbz2.a. EXITING."
+	exit
+    fi
+else
+    LOCAL_BZ2=`dirname $LOCAL_BZ2_A`/..
 fi
 
 #clean all external libraries if CLEAN_EXTERNAL_BUILD=1
@@ -33,7 +34,19 @@ if [ -n "$BUILD_X264" ]; then
 	git clone git://git.videolan.org/x264.git
 	cd x264
 	#make and simulate install in local folder
-	./configure --prefix=./temp_x264_install
+	./configure --prefix="$BASE_UL_DIR/$EXTERN_DIR/x264/temp_x264_install"
+	make; make install
+fi
+
+if [ -n "$BUILD_MP3LAME" ]; then
+	cd "$BASE_UL_DIR/$EXTERN_DIR"
+	rm -r -f mp3lame
+	#get and compile latest mp3lame library
+	rm -f lame-3.98.4.tar.gz
+	wget http://sourceforge.net/projects/lame/files/lame/3.98.4/lame-3.98.4.tar.gz/download; tar xzf lame-3.98.4.tar.gz; mv lame-3.98.4 mp3lame;
+	cd mp3lame
+	#make and simulate install in local folder
+	./configure --disable-gtktest --disable-frontend --prefix="$BASE_UL_DIR/$EXTERN_DIR/mp3lame/temp_mp3lame_install"
 	make; make install
 fi
 
@@ -48,7 +61,7 @@ if [ -n "$BUILD_FFMPEG" ]; then
 	#get instead a specific one because allows output video rate resampling
 	#svn -r 21010 checkout svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg
 	cd ffmpeg
-	./configure --enable-gpl --enable-nonfree --enable-version3 --enable-libmp3lame --enable-libx264 --enable-pthreads --extra-cflags=-I../x264/temp_x264_install/include --extra-ldflags=-L../x264/temp_x264_install/lib
+	./configure --enable-gpl --enable-nonfree --enable-version3 --enable-libmp3lame --enable-libx264 --enable-pthreads --extra-cflags="-I../x264/temp_x264_install/include -I../mp3lame/temp_mp3lame_install/include" --extra-ldflags="-L../x264/temp_x264_install/lib -L../mp3lame/temp_mp3lame_install/lib"
 	make
 fi
 
@@ -59,63 +72,64 @@ if [ -n "$BUILD_MHD" ]; then
 	svn --trust-server-cert --non-interactive checkout https://ng.gnunet.org/svn/libmicrohttpd
 	cd libmicrohttpd
 	autoreconf -fi
-	./configure --disable-curl --disable-https --enable-messages --disable-client-side
-	make
+	./configure --disable-curl --disable-https --enable-messages --disable-client-side --prefix="$BASE_UL_DIR/$EXTERN_DIR/libmicrohttpd/temp_mhd_install"
+	make; make install
 fi
 
 if [ -n "$BUILD_SDL" ]; then
 	cd "$BASE_UL_DIR/$EXTERN_DIR"
-	rm -r -r SDL-1.2.14
+	rm -r -r sdl
 	#get and compile SDL lib
 	rm -f SDL-1.2.14.tar.gz
-	wget http://www.libsdl.org/release/SDL-1.2.14.tar.gz; tar xzf SDL-1.2.14.tar.gz
-	cd SDL-1.2.14
+	wget http://www.libsdl.org/release/SDL-1.2.14.tar.gz; tar xzf SDL-1.2.14.tar.gz; mv SDL-1.2.14 sdl
+	cd sdl
 	#make and simulate install in local folder
-	./configure --disable-video-directfb --prefix="$BASE_UL_DIR/$EXTERN_DIR/SDL-1.2.14/temp_sdl_install"
+	./configure --disable-video-directfb --prefix="$BASE_UL_DIR/$EXTERN_DIR/sdl/temp_sdl_install"
 	make; make install
 fi
 
 if [ -n "$BUILD_SDLIMAGE" ]; then
 	cd "$BASE_UL_DIR/$EXTERN_DIR"
-	rm -r -r SDL_image-1.2.10
+	rm -r -r sdlimage
 	#get and compile SDLIMAGE lib
 	rm -f SDL_image-1.2.10.tar.gz
-	wget http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.10.tar.gz; tar xzf SDL_image-1.2.10.tar.gz
-	cd SDL_image-1.2.10
+	wget http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.10.tar.gz; tar xzf SDL_image-1.2.10.tar.gz; mv SDL_image-1.2.10 sdlimage
+	cd sdlimage
 	#make and simulate install in local folder
-	./configure --prefix="$BASE_UL_DIR/$EXTERN_DIR/SDL_image-1.2.10/temp_sdlimage_install"
+	./configure --prefix="$BASE_UL_DIR/$EXTERN_DIR/sdlimage/temp_sdlimage_install"
 	make; make install
 fi
 
 if [ -n "$BUILD_CURL" ]; then
 	cd "$BASE_UL_DIR/$EXTERN_DIR"
-	rm -r -r curl-7.21.0
+	rm -r -r curl
 	#get and compile CURL lib
 	rm -f curl-7.21.0.tar.bz2
-	wget http://curl.haxx.se/download/curl-7.21.0.tar.bz2; tar xjf curl-7.21.0.tar.bz2
-	cd curl-7.21.0
+	wget http://curl.haxx.se/download/curl-7.21.0.tar.bz2; tar xjf curl-7.21.0.tar.bz2; mv curl-7.21.0 curl
+	cd curl
 	#make and simulate install in local folder
-./configure --disable-ldap --without-libssh2 --without-ssl --without-krb4 --enable-static --disable-shared --without-zlib --without-libidn --prefix="$BASE_UL_DIR/$EXTERN_DIR/curl-7.21.0/temp_curl_install"
+	./configure --disable-ldap --without-libssh2 --without-ssl --without-krb4 --enable-static --disable-shared --without-zlib --without-libidn --prefix="$BASE_UL_DIR/$EXTERN_DIR/curl/temp_curl_install"
 	make; make install
 fi
 
 #set needed paths to external libraries
 echo "-----"
-LOCAL_X264="$BASE_UL_DIR/$EXTERN_DIR/x264"
+LOCAL_X264="$BASE_UL_DIR/$EXTERN_DIR/x264/temp_x264_install"
 echo "path for X264 dependancy set to $LOCAL_X264"
+LOCAL_MP3LAME="$BASE_UL_DIR/$EXTERN_DIR/mp3lame/temp_mp3lame_install"
+echo "path for MP3LAME dependancy set to $LOCAL_MP3LAME"
 LOCAL_FFMPEG="$BASE_UL_DIR/$EXTERN_DIR/ffmpeg"
 echo "path for FFMPEG dependancy set to $LOCAL_FFMPEG"
-LOCAL_MHD="$BASE_UL_DIR/$EXTERN_DIR/libmicrohttpd"
+LOCAL_MHD="$BASE_UL_DIR/$EXTERN_DIR/libmicrohttpd/temp_mhd_install"
 echo "path for LIBMICROHTTPD dependancy set to $LOCAL_MHD"
-LOCAL_ABS_SDL="$BASE_UL_DIR/$EXTERN_DIR/SDL-1.2.14/temp_sdl_install"
+LOCAL_ABS_SDL="$BASE_UL_DIR/$EXTERN_DIR/sdl/temp_sdl_install"
 echo "path for SDL dependancy set to $LOCAL_ABS_SDL"
-LOCAL_SDLIMAGE="$BASE_UL_DIR/$EXTERN_DIR/SDL_image-1.2.10/temp_sdlimage_install"
+LOCAL_SDLIMAGE="$BASE_UL_DIR/$EXTERN_DIR/sdlimage/temp_sdlimage_install"
 echo "path for SDLIMAGE dependancy set to $LOCAL_SDLIMAGE"
-LOCAL_CURL="$BASE_UL_DIR/$EXTERN_DIR/curl-7.21.0/temp_curl_install"
+LOCAL_CURL="$BASE_UL_DIR/$EXTERN_DIR/curl/temp_curl_install"
 echo "path for CURL dependancy set to $LOCAL_CURL"
 
 echo "path for BZ2 dependancy is set to $LOCAL_BZ2"
-echo "path for MP3LAME dependancy is set to $LOCAL_MP3LAME"
 echo "-----"
 
 #compile the UL external applications
@@ -124,29 +138,54 @@ echo "----------------COMPILING CHUNKER STREAMER"
 cd "$BASE_UL_DIR"
 cd chunker_streamer
 if [ -d "$BASE_UL_DIR/../../3RDPARTY-LIBS/libconfuse" ]; then
-	LOCAL_CONFUSE="$BASE_UL_DIR/../../3RDPARTY-LIBS/libconfuse"
-	echo "found LIBCONFUSE in $LOCAL_CONFUSE"
+    LOCAL_CONFUSE="$BASE_UL_DIR/../../3RDPARTY-LIBS/libconfuse"
+    echo "found LIBCONFUSE in $LOCAL_CONFUSE"
 else
-	LOCAL_CONFUSE=`locate libconfuse.a`
-	if [ "$LOCAL_CONFUSE" = "" ]; then
-		echo "you dont have libconfue built in 3DPRTY LIBS of GRAPES nor in system"
-		echo "setting path for it to default /usr/lib/libconfuse.a"
-		LOCAL_CONFUSE="/usr/lib/libconfuse.a"
+    LOCAL_CONFUSE_A=`locate libconfuse.a`
+    if [ "$LOCAL_CONFUSE_A" = "" ]; then
+        if [ -f "/usr/lib/libconfuse.a" ]; then
+	    echo "You have file libconfuse.a in default system"
+	    echo "setting path for it to default /usr/lib/libconfuse.a"
+	    LOCAL_CONFUSE="/usr"
+	else
+	    echo "you seem not to have file libconfuse.a. EXITING."
+	    exit
 	fi
+    else
+	LOCAL_CONFUSE=`dirname $LOCAL_CONFUSE_A`/..
+    fi
 fi
 make clean
-LOCAL_X264=$LOCAL_X264 LOCAL_FFMPEG=$LOCAL_FFMPEG LOCAL_BZ2=$LOCAL_BZ2 LOCAL_MP3LAME=$LOCAL_MP3LAME LOCAL_CURL=$LOCAL_CURL LOCAL_CONFUSE=$LOCAL_CONFUSE make
+LOCAL_X264=$LOCAL_X264 LOCAL_MP3LAME=$LOCAL_MP3LAME LOCAL_FFMPEG=$LOCAL_FFMPEG LOCAL_BZ2=$LOCAL_BZ2 LOCAL_MP3LAME=$LOCAL_MP3LAME LOCAL_CURL=$LOCAL_CURL LOCAL_CONFUSE=$LOCAL_CONFUSE make
 echo "----------------FINISHED COMPILING CHUNKER STREAMER"
 #CHUNKER_PLAYER
 echo "----------------COMPILING CHUNKER PLAYER"
 cd "$BASE_UL_DIR"
 cd chunker_player
 make clean
-LOCAL_X264=$LOCAL_X264 LOCAL_FFMPEG=$LOCAL_FFMPEG LOCAL_BZ2=$LOCAL_BZ2 LOCAL_MP3LAME=$LOCAL_MP3LAME LOCAL_MHD=$LOCAL_MHD LOCAL_ABS_SDL=$LOCAL_ABS_SDL LOCAL_SDLIMAGE=$LOCAL_SDLIMAGE make
+LOCAL_X264=$LOCAL_X264 LOCAL_MP3LAME=$LOCAL_MP3LAME LOCAL_FFMPEG=$LOCAL_FFMPEG LOCAL_BZ2=$LOCAL_BZ2 LOCAL_MP3LAME=$LOCAL_MP3LAME LOCAL_MHD=$LOCAL_MHD LOCAL_ABS_SDL=$LOCAL_ABS_SDL LOCAL_SDLIMAGE=$LOCAL_SDLIMAGE make
 echo "----------------FINISHED COMPILING CHUNKER PLAYER"
 
 #compile a version of offerstreamer with UL enabled
 #static needs fix??
 cd "$BASE_UL_DIR/../OfferStreamer"
 make clean
-ULPLAYER=$BASE_UL_DIR ULPLAYER_EXTERNAL_LIBS=$EXTERN_DIR LIBEVENT_DIR="$BASE_UL_DIR/../../3RDPARTY-LIBS/libevent" ML=1 STATIC= MONL=1 HTTPIO=1 DEBUG=1 make
+if [ -d "$BASE_UL_DIR/../../3RDPARTY-LIBS/libevent" ]; then
+    LOCAL_EVENT="$BASE_UL_DIR/../../3RDPARTY-LIBS/libevent"
+    echo "found LIBEVENT in $LOCAL_EVENT"
+else
+    LOCAL_EVENT_A=`locate libevent.a`
+    if [ "$LOCAL_EVENT_A" = "" ]; then
+        if [ -f "/usr/lib/libevent.a" ]; then
+	    echo "You have file libevent.a in default system"
+	    echo "setting path for it to default /usr/lib/libevent.a"
+	    LOCAL_EVENT="/usr"
+	else
+	    echo "you seem not to have file libevent.a. EXITING."
+	    exit
+	fi
+    else
+	LOCAL_EVENT=`dirname $LOCAL_EVENT_A`/..
+    fi
+fi
+ULPLAYER=$BASE_UL_DIR ULPLAYER_EXTERNAL_LIBS=$EXTERN_DIR LIBEVENT_DIR=$LOCAL_EVENT ML=1 STATIC= MONL=1 HTTPIO=1 DEBUG=1 make
