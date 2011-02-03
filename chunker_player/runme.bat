e8bcd06dd0e9cadf1ff94409f9e0a1fbab97a8fd
MarcoMellia <MarcoMellia@c54fac2e-1815-407e-9b96-088d06743913>
2011-02-03 10:04:32 +0000

    changed the runme.[sh|bat] and channels.conf to remove the workshoplive channel and to default to BBC channel
    git-svn-id: https://repository.napa-wine.eu/svn/napawine-software/trunk/Applications/StreamerPlayerChunker@2505 c54fac2e-1815-407e-9b96-088d06743913

--- chunker_player/runme.bat
+++ chunker_player/runme.bat
@@ -1,3 +1,3 @@
 taskkill /F /FI "IMAGENAME eq chunker_player*" /IM * /T
 taskkill /F /FI "IMAGENAME eq winestreamer-ml-monl-*" /IM * /T
-chunker_player.exe -q 50 -c NAPA-WINE_workshop -p 6109
+chunker_player.exe -q 50 -c TV1_NAPA_BBC_ALTO -p 6109
--- chunker_player/runme.sh
+++ chunker_player/runme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 killall chunker_player
 pkill winestreamer-ml-monl-
-./chunker_player -q 50 -c NAPA-WINE_workshop -p 6109 2>/dev/null
+./chunker_player -q 50 -c TV1_NAPA_BBC_ALTO -p 6109 2>/dev/null
